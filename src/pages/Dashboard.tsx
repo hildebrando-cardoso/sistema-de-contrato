d2a70413fd497d7785c8ce9d95eb4c560410a235
Hildebrando Cardoso <61070954+hildebrando-cardoso@users.noreply.github.com>
2025-08-06 22:27:49 -0300

    fix: remove console.log e confirma funcionalidade do botão novo contrato

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -16,7 +16,6 @@ export const Dashboard = () => {
   const navigate = useNavigate();
 
   const handleNewContract = () => {
-    console.log('Navegando para /contract');
     navigate('/contract');
   };
 
